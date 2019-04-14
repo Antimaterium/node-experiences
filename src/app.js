a027f1ab8182c706926dba9085ca7e045cbc61a7
Antimaterium <vitorafonso33@gmail.com>
2019-04-14 18:16:11 -0300

    Code refactor

--- src/app.js
+++ src/app.js
@@ -1,22 +1,3 @@
-'use strict';
+const app = require('./helpers/App');
 
-const cors = require('cors');
-const helmet = require('helmet');
-const express = require('express');
-require('./config/config');
-
-const { normalizePort } = require('./utils/utils');
-
-const PORT = normalizePort(process.env.PORT || 3000);
-const app = express();
-
-app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-app.use(cors());
-
-app.use('/', require('./routes'));
-
-app.listen( PORT, () => console.log(`Listen on port ${PORT}`));
+app.listen();
---
+++ src/helpers/App.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const cors = require('cors');
+const helmet = require('helmet');
+const express = require('express');
+
+const { normalizePort } = require('../utils/utils');
+require('../config/config');
+
+const App = function App() {
+  this._app = express();
+  this._port = normalizePort(process.env.PORT || 3000);
+  this._middlewares();
+  this._routes();
+}
+
+App.prototype.listen = function listen() {
+  this._app.listen( this._port, () => console.log(`Listen on port ${this._port}`));
+}
+
+App.prototype._middlewares = function _middlewares() {
+  this._app.use(helmet());
+  this._app.use(express.json());
+  this._app.use(express.urlencoded({
+    extended: true
+  }));
+  this._app.use(cors());
+}
+
+App.prototype._routes = function _routes() {
+  this._app.use('/', require('../routes'));
+}
+
+module.exports = new App();
